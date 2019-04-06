7c20981a9e8f83ae2474c484f4b3de9e5410fdec
Jordan Rabideau <jordanmrabideau@gmail.com>
2019-04-06 13:08:56 -0400

    Updates

---
+++ precache-manifest.6659a44a838aa7b4154de8f3e24bbb31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "19ac4c866bc8f01ae633",
+    "url": "/clicky-game/static/js/main.7a2fcbaf.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "19ac4c866bc8f01ae633",
+    "url": "/clicky-game/static/css/main.efdb9961.chunk.css"
+  },
+  {
+    "revision": "43f95e02de46fadfcfec5329e2255677",
+    "url": "/clicky-game/index.html"
+  }
+];
